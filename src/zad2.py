740a060c0be313fc4f41149a863c47322d51b7fd
Maciej Witkowski <maciej.jan.witkowski@gmail.com>
2020-11-02 18:48:52 +0100

    refactor - zad2/test1

--- src/zad2.py
+++ src/zad2.py
@@ -1,4 +1,15 @@
 class RomanNumerals:
+    def __init__(self):
+        self.base = {
+            1: 'I',
+            5: 'V',
+            10: 'X',
+            50: 'L',
+            100: 'C',
+            500: 'D',
+            1000: 'M'
+        }
+
     def roman(self, num):
         if num == 1:
-            return "I"
+            return self.base[1]
