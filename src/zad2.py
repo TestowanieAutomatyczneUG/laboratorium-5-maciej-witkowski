221a3d628a69b000fdcc80b8c94e381b229790be
Maciej Witkowski <maciej.jan.witkowski@gmail.com>
2020-11-02 20:23:09 +0100

    refactor - zad2/test17

--- src/zad2.py
+++ src/zad2.py
@@ -19,6 +19,8 @@ class RomanNumerals:
             return self.base[5]+((partOfNum - 5)*self.base[1])
         elif partOfNum == 9:
             return self.base[1]+self.base[10]
+        else:
+            return ""
 
     def tens(self, partOfNum):
         if 1 <= partOfNum <= 3:
@@ -29,6 +31,20 @@ class RomanNumerals:
             return self.base[50]+((partOfNum - 5)*self.base[10])
         elif partOfNum == 9:
             return self.base[10]+self.base[100]
+        else:
+            return ""
+
+    def hundreds(self, partOfNum):
+        if 1 <= partOfNum <= 3:
+            return self.base[100] * partOfNum
+        elif partOfNum == 4:
+            return self.base[100] + self.base[500]
+        elif 5 <= partOfNum <= 8:
+            return self.base[500] + ((partOfNum - 5) * self.base[100])
+        elif partOfNum == 9:
+            return self.base[100] + self.base[1000]
+        else:
+            return ""
 
     def roman(self, num):
         divided = [int(i) for i in str(num)]
@@ -36,13 +52,6 @@ class RomanNumerals:
             return self.ones(divided[0])
         elif num < 100:
             return self.tens(divided[0])+self.ones(divided[1])
-        elif num == 141:
-            return self.base[100]+self.tens(4)+self.ones(1)
-        elif num == 163:
-            return self.base[100]+self.tens(6)+self.ones(3)
-        elif num == 402:
-            return self.base[100]+self.base[500]+self.ones(2)
-        elif num == 575:
-            return self.base[500]+self.tens(7)+self.ones(5)
-        elif num == 911:
-            return self.base[100]+self.base[1000]+self.tens(1)+self.ones(1)
+        elif num < 1000:
+            return self.hundreds(divided[0])+self.tens(divided[1])+self.ones(divided[2])
+
