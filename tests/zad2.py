0e866297b9ff59b729adbb0411b52760884514ba
Maciej Witkowski <maciej.jan.witkowski@gmail.com>
2020-11-02 18:54:30 +0100

    red - zad2/test3

--- tests/zad2.py
+++ tests/zad2.py
@@ -9,7 +9,6 @@ class RomanNumeralsTest(unittest.TestCase):
     def test_2_is_two_i_s(self):
         self.assertEqual(self.romanNumerals.roman(2), "II")
 
-    @unittest.skip("not implemented")
     def test_3_is_three_i_s(self):
         self.assertEqual(self.romanNumerals.roman(3), "III")
 
