55757a84a390628c7172f0bb8450b1939bff843d
Maciej Witkowski <maciej.jan.witkowski@gmail.com>
2020-11-02 18:59:29 +0100

    green - zad2/test4

--- src/zad2.py
+++ src/zad2.py
@@ -17,3 +17,6 @@ class RomanNumerals:
             return self.base[1]+self.base[1]
         elif num == 3:
             return self.base[1]+self.base[1]+self.base[1]
+        elif num == 4:
+            return self.base[1]+self.base[5]
+
