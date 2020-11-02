640d321fded9e154613618464993d5fa41e752b3
Maciej Witkowski <maciej.jan.witkowski@gmail.com>
2020-11-02 20:13:44 +0100

    green - zad2/test15

--- src/zad2.py
+++ src/zad2.py
@@ -40,3 +40,5 @@ class RomanNumerals:
             return self.base[100]+self.tens(4)+self.ones(1)
         elif num == 163:
             return self.base[100]+self.tens(6)+self.ones(3)
+        elif num == 402:
+            return self.base[100]+self.base[500]+self.ones(2)
