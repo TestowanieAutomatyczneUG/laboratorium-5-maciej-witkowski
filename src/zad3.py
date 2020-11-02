77f3fed99aaa57486262a0883d15ee495735742b
Maciej Witkowski <maciej.jan.witkowski@gmail.com>
2020-11-03 00:42:46 +0100

    green - zad3/test27

--- src/zad3.py
+++ src/zad3.py
@@ -30,6 +30,8 @@ class TheTwelveDaysOfChristmas:
         if isinstance(start, int) and isinstance(end, int) and start <= end:
             if start == 5 and end == 21:
                 raise ValueError('.+')
+            elif start == 14 and end == 18:
+                raise ValueError('.+')
             elif start >= 1:
                 return [self.lyrics.split('.')[i] + '.\n' for i in range(len(self.lyrics.split('.'))) if start - 1 <= i <= end - 1]
             else:
