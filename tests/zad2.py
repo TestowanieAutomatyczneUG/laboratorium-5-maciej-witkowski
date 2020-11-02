520324e9f43c5971ea858ad5f0e91ada94a5d6a3
Maciej Witkowski <maciej.jan.witkowski@gmail.com>
2020-11-02 19:42:18 +0100

    red - zad2/test9

--- tests/zad2.py
+++ tests/zad2.py
@@ -27,7 +27,6 @@ class RomanNumeralsTest(unittest.TestCase):
     def test_20_is_two_x_s(self):
         self.assertEqual(self.romanNumerals.roman(27), "XXVII")
 
-    @unittest.skip("not implemented")
     def test_48_is_not_50_2_but_rather_40_8(self):
         self.assertEqual(self.romanNumerals.roman(48), "XLVIII")
 
