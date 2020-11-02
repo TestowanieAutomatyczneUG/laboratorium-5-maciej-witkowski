41a0462a042281ade27f05f17793aee2a452580b
Maciej Witkowski <maciej.jan.witkowski@gmail.com>
2020-11-02 17:43:12 +0100

    refactor - zad1/test9

--- src/zad1.py
+++ src/zad1.py
@@ -1,20 +1,10 @@
 class Hamming:
     def distance(self, a, b):
-        count = 0
-        if a == "" and b == "":
-            return count
-        elif a == b:
-            return count
-        elif len(a) == len(b):
+        if len(a) == len(b):
+            count = 0
             for i in range(len(a)):
                 if a[i] != b[i]:
                     count += 1
             return count
-        elif a == "AATG" and b == "AAA":
-            raise ValueError('.+')
-        elif a == "ATA" and b == "AGTG":
-            raise ValueError('.+')
-        elif a == "" and b == "G":
-            raise ValueError('.+')
-        elif a == "G" and b == "":
+        else:
             raise ValueError('.+')
