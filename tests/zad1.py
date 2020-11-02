ba740c98e328a75219b2d7d9ed4f81feddbb50e1
Maciej Witkowski <maciej.jan.witkowski@gmail.com>
2020-11-02 17:31:58 +0100

    red - zad1/test9

--- tests/zad1.py
+++ tests/zad1.py
@@ -30,7 +30,6 @@ class HammingTest(unittest.TestCase):
         with self.assertRaisesWithMessage(ValueError):
             self.hamming.distance("", "G")
 
-    @unittest.skip("not implemented")
     def test_disallow_right_empty_strand(self):
         with self.assertRaisesWithMessage(ValueError):
             self.hamming.distance("G", "")
